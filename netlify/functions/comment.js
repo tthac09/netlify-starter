e9aa771783349ac7949017c84acb26aab00679e6
tthac09 <93647274+tthac09@users.noreply.github.com>
2023-10-23 23:59:55 +0800

    Update comment.js

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -10,9 +10,9 @@ const app = Waline({
   comment.status = think.isEmpty(userInfo) ? 'waiting' : 'approved';
   },
   mailSubjectAdmin: "tthac09's blog 有新评论啦",
-  mailTemplateAdmin："您在 [tthac09.github.io]tthac09's_blog 上的文章有新评论啦",
+  mailTemplateAdmin："您在 tthac09's_blog 上的文章有新评论啦",
   mailSubject: "您在 tthac09's blog 上的评论有新回复啦",
-  mailTemplate："您在 [tthac09.github.io]tthac09's_blog 上的评论有新回复啦，快来看看呀",
+  mailTemplate："您在 tthac09's_blog 上的评论有新回复啦，快来看看呀",
 });
 
 module.exports.handler = serverless(http.createServer(app));
